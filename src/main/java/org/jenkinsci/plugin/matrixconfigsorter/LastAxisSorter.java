62dde0ef9a8d4c7242a79d42180f6f99dccb2e79
lucinka <lucinka@localhost.localdomain>
2011-11-21 08:29:35 +0100

    Add class description.

--- src/main/java/org/jenkinsci/plugin/matrixconfigsorter/LastAxisSorter.java
+++ src/main/java/org/jenkinsci/plugin/matrixconfigsorter/LastAxisSorter.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2011 lucinka.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jenkinsci.plugin.matrixconfigsorter;
 
 import hudson.Extension;
@@ -29,16 +6,14 @@ import hudson.matrix.MatrixConfiguration;
 import hudson.matrix.MatrixConfigurationSorter;
 import hudson.matrix.MatrixConfigurationSorterDescriptor;
 import hudson.matrix.MatrixProject;
-import hudson.matrix.listeners.MatrixBuildListener;
 import hudson.util.FormValidation;
-import java.util.Comparator;
 import java.util.List;
-import jenkins.model.Jenkins;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
+ * Sort configurations {@link MatrixConfiguration}  of matrix job {@link MatrixProject} by order of values in last axis.
  * 
- * @author lucinka
+ * @author Lucie Votypkova
  */
 @Extension
 public class LastAxisSorter extends MatrixConfigurationSorter{
